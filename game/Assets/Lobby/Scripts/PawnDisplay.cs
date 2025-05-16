b83d7fd16225232a95199f6e27c8ab2f416621f7
Ioana <ioana.agheorghiesei@student.uaic.ro>
2025-05-16 16:24:09 +0300

    animatie selectie pion

--- game/Assets/Lobby/Scripts/PawnDisplay.cs
+++ game/Assets/Lobby/Scripts/PawnDisplay.cs
@@ -2,19 +2,21 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using System.Collections;
+using static System.Net.Mime.MediaTypeNames;
 
 public class PawnDisplay : MonoBehaviour
 {
-    [SerializeField] private Image pawnImage;
+    [SerializeField] private UnityEngine.UI.Image pawnImage;
     [SerializeField] public Button clickableButton;
 
+    private Coroutine blinkCoroutine;
+
+    [SerializeField] private UnityEngine.UI.Image highlightOverlay;
 
     public void Setup(Sprite pawnSprite)
     {
         pawnImage.sprite = pawnSprite;
-   }
-
-    private Coroutine blinkCoroutine;
+    }
 
     public void SetBlinking(bool shouldBlink)
     {
@@ -32,7 +34,7 @@ public class PawnDisplay : MonoBehaviour
                 blinkCoroutine = null;
             }
 
-            pawnImage.color = Color.white;
+            SetOverlayAlpha(0f);
             clickableButton.interactable = false;
         }
     }
@@ -41,11 +43,41 @@ public class PawnDisplay : MonoBehaviour
     {
         while (true)
         {
-            pawnImage.color = new Color(1f, 1f, 1f, 0.3f);
-            yield return new WaitForSeconds(0.3f);
-            pawnImage.color = new Color(1f, 1f, 1f, 1f);
-            yield return new WaitForSeconds(0.3f);
+            yield return FadeOverlay(0f, 0.3f); // fade in to 80%
+            yield return FadeOverlay(0.8f, 0.3f); // fade out to 0%
         }
     }
+
+    private IEnumerator FadeOverlay(float targetAlpha, float duration)
+    {
+        float startAlpha = highlightOverlay.color.a;
+        float time = 0f;
+
+        while (time < duration)
+        {
+            float t = time / duration;
+            float alpha = Mathf.Lerp(startAlpha, targetAlpha, t);
+            SetOverlayAlpha(alpha);
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        SetOverlayAlpha(targetAlpha);
+    }
+
+    private void SetOverlayAlpha(float alpha)
+    {
+        Color c = highlightOverlay.color;
+        c.a = alpha;
+        highlightOverlay.color = c;
+    }
+
+    private void Awake()
+    {
+        if (highlightOverlay == null)
+            highlightOverlay = transform.GetComponentInChildren<UnityEngine.UI.Image>(includeInactive: true);
+    }
+
+
 
 }
